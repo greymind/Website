cb0237133f4607e5b984252c844384ce29d87fa0
Balakrishnan (Balki) Ranganathan <balki@live.com>
2017-04-30 16:50:58 +0200

    Render full resume, bring in core layout for app and menus

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ import "../styles/main.less";
 import { Home } from "./Home";
 import { Resume, IResumeProps } from "./Resume";
 
-import * as ResumeData from "./resume.json";
+import * as ResumeData from "../../data/resume.json";
 
 export interface IAppState {
     resumeData: IResumeProps;
@@ -20,9 +20,47 @@ export class App extends React.Component<undefined, IAppState> {
 
     render() {
         return (
-            <div className="nav">
-                <Home />
-                <Resume {...this.state.resumeData} />
+            <div>
+                <nav className="navbar navbar-default navbar-fixed-top" bootstrap-active-link>
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+                                aria-expanded="false">
+                                <span className="sr-only">Toggle navigation</span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                            <a className="pull-left" href="#/home"><img src="../images/Logo-Transparent.png" /></a>
+                        </div>
+                        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul className="nav navbar-nav">
+                                <li className="active"><a href="#/home">Home <span className="sr-only">(current)</span></a></li>
+                                <li className="dropdown">
+                                    <a href="" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Projects<span className="caret"></span></a>
+                                    <ul className="dropdown-menu">
+                                        <li className="dropdown-header">Maya Tools</li>
+                                        <li><a target="_blank" href="https://github.com/greymind/Sequencer">Sequencer</a></li>
+                                        <li><a target="_blank" href="https://github.com/greymind/Sequencer">ModelDef Exporter</a></li>
+                                        <li><a target="_blank" href="https://github.com/greymind/Sequencer">Scene Exporter</a></li>
+                                        <li><a target="_blank" href="https://github.com/greymind/Sequencer">Custom Data Exporter</a></li>
+                                        <li className="dropdown-header">Audio/Video Editing</li>
+                                        <li><a target="_blank" href="https://github.com/greymind/ClipSynth">ClipSynth</a></li>
+                                        <li className="dropdown-header">Pipeline Tools</li>
+                                        <li><a target="_blank" href="https://github.com/greymind/DatabaseConverters">Database Converters</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                            <ul className="nav navbar-nav navbar-right">
+                                <li><a href="#/resume">Resume</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <div className="container">
+                    <Home />
+                    <Resume {...this.state.resumeData} />
+                </div>
             </div>
         )
     }
--- client/src/Resume.tsx
+++ client/src/Resume.tsx
@@ -55,8 +55,85 @@ export class Resume extends React.Component<IResumeProps, IResumeState> {
     render() {
         return (
             <div>
-                {this.props.basics.name}
-                {this.props.basics.email}
+                <div className="jumbotron">
+                    <div><strong>{this.props.basics.name}</strong></div>
+                    <div><em>{this.props.basics.title}</em></div>
+                    <div>
+                        <div>{this.props.basics.address}</div>
+                        <div>{this.props.basics.city}, {this.props.basics.state}, {this.props.basics.zip}</div>
+                        <div>{this.props.basics.email}</div>
+                        <div>{this.props.basics.phone}</div>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="panel panel-default">
+                        <div className="panel-heading">
+                            <a role="button" data-toggle="collapse" data-target="#SkillsList">
+                                <h3 className="panel-title">
+                                    Skills
+                                </h3>
+                            </a>
+                        </div>
+                        <ul className="list-group panel-collapse collapse in">
+                            {this.props.skills.map((skill, skillIndex) =>
+                                <li key={skillIndex} className="list-group-item">
+                                    <strong>{skill.category} | </strong>
+                                    <em>
+                                        {skill.items.map((item, itemIndex, itemArray) =>
+                                            <span key={itemIndex}>
+                                                <span>{item}</span>
+                                                {itemIndex !== itemArray.length - 1 ? <span>, </span> : <span></span>}
+                                            </span>
+                                        )}
+                                    </em>
+                                </li>
+                            )}
+                        </ul>
+                    </div>
+
+                    <div className="panel panel-default">
+                        <div className="panel-heading">
+                            <a role="button" data-toggle="collapse" data-target="#ExperienceList">
+                                <h3 className="panel-title">
+                                    Experience
+                                </h3>
+                            </a>
+                        </div>
+                        <ul className="list-group panel-collapse collapse in">
+                            {this.props.experience.map((experience, experienceIndex) =>
+                                <li key={experienceIndex} className="list-group-item">
+                                    <div><strong>{experience.title}</strong>
+                                        <br />{experience.organization.division} @ <em>{experience.organization.name}</em>
+                                        <br />{experience.from.month} {experience.from.year} - {experience.to.month} {experience.to.year},
+                        {experience.location}
+                                    </div>
+                                    <div><em>{experience.description}</em></div>
+                                </li>
+                            )}
+                        </ul>
+                    </div>
+
+                    <div className="panel panel-default">
+                        <div className="panel-heading">
+                            <a role="button" data-toggle="collapse" data-target="#EducationList">
+                                <h3 className="panel-title">
+                                    Education
+                    </h3>
+                            </a>
+                        </div>
+                        <ul className="list-group panel-collapse collapse in">
+                            {this.props.education.map(education =>
+                                <li className="list-group-item">
+                                    <div><strong>{education.degree}</strong>
+                                        <br />{education.school}
+                                        <br /><em>{education.focus}</em>
+                                    </div>
+                                </li>)}
+                        </ul>
+                    </div>
+                </div>
+
             </div>
         );
     }
