19d0bf0946ebe38c4e613184118a6600777fe52f
Balakrishnan (Balki) Ranganathan <balki@live.com>
2017-05-02 01:15:48 +0200

    Fixed active link!

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -9,6 +9,7 @@ import {
 
 import * as Projects from "../../data/projects.json"
 import { ICategory, IProject, IProjectMenuItem } from "./projects/interfaces";
+import { ListItemNavLink } from "./framework/ListItemNavLink";
 
 const categories: ICategory[] = Projects;
 const menuItems = _.flatMap(categories, (category) => {
@@ -26,41 +27,36 @@ const menuItems = _.flatMap(categories, (category) => {
     }));
 });
 
-export const Header = () => {
-    return (
-        <div>
-            <nav className="navbar navbar-default navbar-fixed-top">
-                <div className="container-fluid">
-                    <div className="navbar-header">
-                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
-                            aria-expanded="false">
-                            <span className="sr-only">Toggle navigation</span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                        </button>
-                        <NavLink className="pull-left greymind-logo" to="/home"></NavLink>
+export class Header extends React.Component<undefined, undefined> {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-default navbar-fixed-top">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <NavLink className="pull-left greymind-logo" to="/home"></NavLink>
+                        </div>
+                        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul className="nav navbar-nav">
+                                <ListItemNavLink to="/home">Home</ListItemNavLink>
+                                <li className="dropdown">
+                                    <a className="dropdown-toggle" data-toggle="dropdown" role="button">Projects<span className="caret"></span></a>
+                                    <ul className="dropdown-menu">
+                                        {menuItems.map((menuItem, menuItemIndex) =>
+                                            menuItem.isCategory
+                                                ? <li key={menuItemIndex} className="dropdown-header">{menuItem.name}</li>
+                                                : <li key={menuItemIndex}><a target="_blank" href={menuItem.url}>{menuItem.name}</a></li>
+                                        )}
+                                    </ul>
+                                </li>
+                            </ul>
+                            <ul className="nav navbar-nav navbar-right">
+                                <ListItemNavLink to="/resume">Resume</ListItemNavLink>
+                            </ul>
+                        </div>
                     </div>
-                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul className="nav navbar-nav">
-                            <li className="active"><NavLink to="/home">Home <span className="sr-only">(current)</span></NavLink></li>
-                            <li className="dropdown">
-                                <a href="" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Projects<span className="caret"></span></a>
-                                <ul className="dropdown-menu">
-                                    {menuItems.map((menuItem, menuItemIndex) =>
-                                        menuItem.isCategory
-                                            ? <li key={menuItemIndex} className="dropdown-header">{menuItem.name}</li>
-                                            : <li key={menuItemIndex}><a target="_blank" href={menuItem.url}>{menuItem.name}</a></li>
-                                    )}
-                                </ul>
-                            </li>
-                        </ul>
-                        <ul className="nav navbar-nav navbar-right">
-                            <li><NavLink to="/resume">Resume</NavLink></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    );
+                </nav>
+            </div>
+        );
+    }
 }
---
+++ client/src/framework/ListItemNavLink.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { NavLinkProps, NavLink } from "react-router-dom";
+import { RouterChildContext } from "react-router";
+
+interface IListItemNavLinkState {
+    isActive: boolean;
+}
+
+interface IListItemNavLinkContext extends RouterChildContext<any> {
+
+}
+
+export class ListItemNavLink extends React.Component<NavLinkProps, IListItemNavLinkState> {
+    state: IListItemNavLinkState = {
+        isActive: false
+    }
+
+    context: IListItemNavLinkContext
+
+    static contextTypes = {
+        router: React.PropTypes.object.isRequired
+    }
+
+    isActive = () => {
+        return this.props.to == this.context.router.route.location.pathname;
+    }
+
+    render() {
+        return (
+            <li className={this.isActive() ? "active" : ""}>
+                <NavLink {...this.props}>
+                    {this.props.children}
+                </NavLink>
+            </li>
+        )
+    }
+}
