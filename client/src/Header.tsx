5b7439895abcff9e6f4da6f0316b334e57c0f1ec
Balakrishnan (Balki) Ranganathan <balki@live.com>
2017-05-02 01:21:42 +0200

    Restore responsive header collapse handlebar thingy

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -34,9 +34,15 @@ export class Header extends React.Component<undefined, undefined> {
                 <nav className="navbar navbar-default navbar-fixed-top">
                     <div className="container-fluid">
                         <div className="navbar-header">
+                            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#g-header-navbar-collapse" aria-expanded="false">
+                                <span className="sr-only">Toggle navigation</span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
                             <NavLink className="pull-left greymind-logo" to="/home"></NavLink>
                         </div>
-                        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <div className="collapse navbar-collapse" id="g-header-navbar-collapse">
                             <ul className="nav navbar-nav">
                                 <ListItemNavLink to="/home">Home</ListItemNavLink>
                                 <li className="dropdown">
@@ -56,7 +62,7 @@ export class Header extends React.Component<undefined, undefined> {
                         </div>
                     </div>
                 </nav>
-            </div>
+            </div >
         );
     }
 }
