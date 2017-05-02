8af242c4fa9db9e6dfb972ec0f3f0dc4b734200a
Balakrishnan (Balki) Ranganathan <balki@live.com>
2017-05-02 20:43:48 +0200

    Crowd-sourcing :P

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -4,6 +4,7 @@ export const Home: React.StatelessComponent<undefined> = () => {
     return (
         <div >
             <h1>Welcome to Greymind!</h1>
+            <p>Nothing to show here, any ideas? :P</p>
         </div >
     );
 };
