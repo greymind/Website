4f6dacb69cdcc1f9252acb8e7f367f9c2a027798
Balakrishnan (Balki) Ranganathan <balki@live.com>
2017-05-02 02:45:10 +0200

    One step better!

--- client/src/framework/ListItemNavLink.tsx
+++ client/src/framework/ListItemNavLink.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import * as PropTypes from "prop-types";
 import { NavLinkProps, NavLink } from "react-router-dom";
-import { RouterChildContext } from "react-router";
+import { RouterChildContext } from "react-router-dom";
+import { matchPath } from "react-router-dom";
 
 interface IListItemNavLinkState {
     isActive: boolean;
@@ -12,27 +13,37 @@ interface IListItemNavLinkContext extends RouterChildContext<any> {
 }
 
 export class ListItemNavLink extends React.Component<NavLinkProps, IListItemNavLinkState> {
+    static defaultProps: Partial<NavLinkProps> = {
+        activeClassName: "active"
+    };
+
     state: IListItemNavLinkState = {
         isActive: false
-    }
+    };
 
-    context: IListItemNavLinkContext
+    context: IListItemNavLinkContext;
 
     static contextTypes = {
         router: PropTypes.object.isRequired
-    }
+    };
 
     isActive = () => {
-        return this.props.to == this.context.router.route.location.pathname;
+        var match: any = matchPath(this.context.router.route.location.pathname, {
+            exact: this.props.exact,
+            strict: this.props.strict,
+            path: this.props.to.toString()
+        });
+
+        return !!match;
     }
 
-    render() {
+    render(): JSX.Element {
         return (
-            <li className={this.isActive() ? "active" : ""}>
+            <li className={this.isActive() ? this.props.activeClassName : ""}>
                 <NavLink {...this.props}>
                     {this.props.children}
                 </NavLink>
             </li>
-        )
+        );
     }
 }
