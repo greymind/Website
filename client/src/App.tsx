ce8197927f396085acb8d204d1e197924e45c193
Balakrishnan (Balki) Ranganathan <balki@live.com>
2017-05-03 21:07:18 +0200

    Back to hash for now, since it fails on reload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import "../styles/main.less";
 
