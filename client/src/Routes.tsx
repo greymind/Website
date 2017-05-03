353689378cb3d267540384b9cc8f4e299c275d63
Balakrishnan (Balki) Ranganathan <balki@live.com>
2017-05-03 03:45:06 +0200

    move props to correct file

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -1,27 +1,22 @@
 import * as React from "react";
 
-import { Route, RouteProps } from "react-router-dom";
+import { IRouteWithSubRoutesProps } from "./framework/RouteWithSubroutes";
 
 import { Home } from "./Home";
 import { Resume } from "./resume/Resume";
 
-export interface IRoutesProps extends RouteProps {
-    component?: any;
-    routes?: IRoutesProps[];
-}
-
-export const Routes: IRoutesProps[] = [
+export const Routes: IRouteWithSubRoutesProps[] = [
     {
         path: "/",
-        component: Home as any,
+        component: Home,
         exact: true,
     },
     {
         path: "/home",
-        component: Home as any,
+        component: Home,
     },
     {
         path: "/resume",
-        component: Resume as any
+        component: Resume
     }
 ];
--- client/src/framework/RouteWithSubRoutes.tsx
+++ client/src/framework/RouteWithSubRoutes.tsx
@@ -1,8 +1,12 @@
 import * as React from "reacT";
-import { IRoutesProps } from "../Routes";
 import { Route, RouteProps } from "react-router-dom";
 
-export const RouteWithSubRoutes: React.StatelessComponent<IRoutesProps> = (route) => {
+export interface IRouteWithSubRoutesProps extends RouteProps {
+    component?: any;
+    routes?: IRouteWithSubRoutesProps[];
+}
+
+export const RouteWithSubRoutes: React.StatelessComponent<IRouteWithSubRoutesProps> = (route) => {
     return (
         <Route path={route.path} render={props => (
             // pass the sub-routes down to keep nesting
